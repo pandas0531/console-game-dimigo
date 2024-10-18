8bf139554112a49ac717520ce444e97833e710d2
pandas <rlajunwon0531@gmail.com>
2024-10-19 08:55:14 +0900

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <time.h>
+
+void start(){
+    FILE *fp = fopen("banner.txt", "r");
+    char str[1000];
+    while(str != EOF){
+        fgets(str, 1, fp);
+        printf("%s", str);
+    }
+}
 
 int main(){
-    int a;
-    scanf("%d", &a);
-    printf("%d", a);
-}//
+    start();
+}
